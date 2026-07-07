9266dbc47f8fce339843b0e0ee882d80b6ededa6
Kostiantyn Spyrydonov <kostiantynspyrydonov@gmail.com>
2026-07-07 19:22:11 +0200

    start building js

---
+++ script.js
@@ -0,0 +1,17 @@
+const countryInput = document.getElementById("country-input")
+const searchBox = document.getElementById("search-box")
+const countryDetailsElem = document.getElementById("country-details")
+const countryName = document.getElementById("country-name")
+const capitalTxt = document.getElementById("capital-city")
+const areaTxt = document.getElementById("area")
+const populationTxt = document.getElementById("population")
+const densityTxt = document.getElementById("pop-density")
+const currencyTxt = document.getElementById("currency")
+const languagesTxt = document.getElementById("languages")
+const continentTxt = document.getElementById("continent")
+const flagImg = document.getElementById("flag")
+const errTxt = document.getElementById("errorTxt")
+
+async function getCountry(countryName) {
+  const res = await fetch(`https://api.restcountries.com/countries/v5/`)
+}
