366b33fbefd235ced8a2bb98c76f0cc18cac49d2
Kostiantyn Spyrydonov <kostiantynspyrydonov@gmail.com>
2026-07-08 14:05:58 +0200

    complete script js and start looking for bugs

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ async function getCountry(countryName) {
   const data = await response.json();
   return data.find(value => value.names.common === countryName) || data[0]
 }
+
+searchBox.addEventListener("submit", async e => {
+  e.preventDefault()
+  if (countryInput.value.trim() === "") {
+    errTxt.textContent = "It is Required To Enter a Country Name to search"
+  } else {
+    errTxt.textContent = ""
+    try {
+      const country = await getCountry(countryInput.value);
+      const { name, capital, area, population, currencies, languages, continents, flags } = country;
+      const currency = Object.values(currencies)[0];
+      countryName.textContent = name.common
+      capitalTxt.textContent = capital.join(", ")
+      areaTxt.textContent = area
+      populationTxt.textContent = population
+      densityTxt.textContent = (population / area).toFixed(2)
+      currencyTxt.textContent = `${currency.name} (${currency.symbol})`
+      languagesTxt.textContent = Object.values(languages).join(", ")
+      continentTxt.textContent = continents.join(", ")
+      flagImg.src = flags.svg
+      if (!countryDetailsElem.classList.contains("active"))
+        countryDetailsElem.classList.add("active")
+    } catch {
+      errTxt.textContent = "Country Not Found"
+    }
+  }
+})
